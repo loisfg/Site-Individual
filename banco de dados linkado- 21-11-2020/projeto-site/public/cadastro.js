c674a39a05231b67c62d0ec1d84375246ede77ab
loisfg <luis.oliveira@bandtec.com.br>
2020-11-21 19:36:39 -0300

    Site com banco linkado e funcionando

---
+++ banco de dados linkado- 21-11-2020/projeto-site/public/cadastro.js
@@ -0,0 +1,36 @@
+function cadastrar() {
+    // aguardar();
+    var formulario = new URLSearchParams(new FormData(form_cadastro));
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        body: formulario
+    }).then(function (response) {
+        
+        if (response.ok) {
+            alert(`Cadastro realizado com sucesso`);
+            window.location.href='logincadastro.html';
+
+        } else {
+
+            console.log('Erro de cadastro!');
+            response.text().then(function (resposta) {
+                div_erro.innerHTML = resposta;
+            });
+            // finalizar_aguardar();
+        }
+    });
+
+    return false;
+}
+
+// function aguardar() {
+//     btn_entrar.disabled = true;
+//     img_aguarde.style.display='block';
+//     div_erro.style.display='none';
+// }
+
+// function finalizar_aguardar() {
+//     btn_entrar.disabled = false;
+//     img_aguarde.style.display='none';
+//     div_erro.style.display='block';
+// }
